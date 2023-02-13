5b8670d2dcbc7f9bd71a67214ce1a4e605eb8c44
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-02-13 09:47:27 +0100

    made changes in the driver's class

--- CardValidator/CardApp.java
+++ CardValidator/CardApp.java
@@ -6,6 +6,8 @@ public class CardApp {
     public static void main(String[] args) {
 
       String cardNumber = "5399831619690403";
+
+                     // TEST CASES
 //        String cardNumber = "45678789264536321212";
 //        String cardNumber ="3756787892645368";
 //        String cardNumber = "6756787892645368";
