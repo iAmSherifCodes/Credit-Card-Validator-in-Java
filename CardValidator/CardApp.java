610bb1e07a07002eb58d475fed0bbb5cd7b90b25
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-02-14 16:31:54 +0100

    Optimize code

--- CardValidator/CardApp.java
+++ CardValidator/CardApp.java
@@ -1,11 +1,15 @@
 package CardValidator;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CardApp {
     public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
 
-      String cardNumber = "5399831619690403";
+      System.out.print("Hello, Kindly Enter Card number to verify: ");
+      String cardNumber = scan.nextLine();
+              //"5399831619690403";
 
                      // TEST CASES
 //        String cardNumber = "45678789264536321212";
